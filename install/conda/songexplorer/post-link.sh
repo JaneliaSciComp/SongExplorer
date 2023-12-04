132749121eb0b17361319e086b8c3cfd5ea7060d
Ben Arthur <arthurb@hhmi.org>
2023-12-04 13:27:13 -0500

    move all pip install commands after conda install

--- install/conda/songexplorer/post-link.sh
+++ install/conda/songexplorer/post-link.sh
@@ -1,10 +1,14 @@
 cat << EOF >> ${PREFIX}/.messages.txt
 ********** IMPORTANT !!! **********
+optionally, to use video in songexplorer cut and paste the following into the command line:
+conda activate songexplorer
+conda install av=8.1 git
+pip3 install -e git+https://github.com/soft-matter/pims.git@7bd634015ecbfeb7d92f9f9d69f8b5bb4686a6b4#egg=pims
 EOF
 
 if [ `uname` == Darwin ] ; then
 cat << EOF >> ${PREFIX}/.messages.txt
-cut and paste the following into the command line to finish the installation:
+either way, to finish the installation cut and paste the following:
 conda activate songexplorer
 pip3 install tensorflow
 EOF
@@ -13,18 +17,8 @@ cat << EOF >> ${PREFIX}/.messages.txt
 pip3 install tensorflow-metal
 EOF
 fi
-cat << EOF >> ${PREFIX}/.messages.txt
-optionally, to use video in songexplorer cut and paste the following too:
-EOF
-else
-cat << EOF >> ${PREFIX}/.messages.txt
-optionally, to use video in songexplorer cut and paste the following into the command line:
-conda activate songexplorer
-EOF
 fi
 
 cat << EOF >> ${PREFIX}/.messages.txt
-conda install av=8.1 git
-pip3 install -e git+https://github.com/soft-matter/pims.git@7bd634015ecbfeb7d92f9f9d69f8b5bb4686a6b4#egg=pims
 ********** IMPORTANT !!! **********
 EOF
