ab3f79418354423ca1c615f63957b89579e0ebac
Ben Arthur <arthurb@hhmi.org>
2024-10-07 18:06:25 -0400

    fix arguments to DROP-FILES-HERE.sh

--- app/DROP-FILES-HERE.sh
+++ app/DROP-FILES-HERE.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${(%):-%N}" )" &> /dev/null && pwd )
 
 export PATH=$SCRIPT_DIR/songexplorer/bin:$PATH
 
-$SCRIPT_DIR/make-predictions.py $argv[2,-1]
+$SCRIPT_DIR/make-predictions.py $argv
